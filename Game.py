cca57592cabd851d7fe09122e560fb92955aa488
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-13 23:57:25 +0300

    ""

--- Game.py
+++ Game.py
@@ -42,6 +42,8 @@ def Your_score(score):
     dis.blit(value, [0, 0])
 
 
+
+
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
