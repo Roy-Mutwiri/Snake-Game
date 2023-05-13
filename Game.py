f4aa134998b823ae31bdfcaddb0d809bc3dde156
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-13 23:50:06 +0300

    ""

--- Game.py
+++ Game.py
@@ -16,8 +16,10 @@ blue = (50, 153, 213)
 dis_width = 600
 dis_height = 400
 
+
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Roy')
+
 
 clock = pygame.time.Clock()
 
