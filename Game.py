320a87aa61f2200a3d1039ba09191408da5d5c03
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-13 23:23:23 +0300

    ""

--- Game.py
+++ Game.py
@@ -3,11 +3,12 @@ import pygame
 pygame.init()
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
-pygame.display.set_caption('Snake game by Edureka')
+pygame.display.set_caption('Snake game by Roy')
 game_over = False
 while not game_over:
     for event in pygame.event.get():
-        print(event)  # prints out all the actions that take place on the screen
+        if event.type == pygame.QUIT:
+            game_over = True
 
 pygame.quit()
 quit()
